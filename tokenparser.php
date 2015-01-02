c060c8c64a71bda9d5da9b4fb936f3b8ba10f20e
Roni Das <kernelroni@yahoo.com>
2015-01-02 15:30:37 +0600

    First commit

---
+++ tokenparser.php
@@ -0,0 +1,35 @@
+<?php
+
+
+// token and its values
+$tokenValues = array();
+$tokenValues['name'] = "MY NAME";
+$tokenValues['company'] = "MY COMPANY";
+$tokenValues['testtoken'] = "TESTTOKEN";
+
+// sample massage body
+$body = "abc {company} def {name} ijk {name} lmn {company} opq {testtoken} rst {notexists} uvw {name} xyz. ";
+echo "before parse <br />";
+echo $body . "<br>";
+
+// pattern
+$pattern = "/{(.*?)}/";
+preg_match_all($pattern, $body, $tokens);
+
+
+	if($tokens){
+		$tokens[0] = array_unique($tokens[0]);
+		$tokens[1] = array_unique($tokens[1]);
+		foreach($tokens[1] as $key => $token){
+			$tokenValue = "";
+				if(isset($tokenValues[$token])){
+					$tokenValue = $tokenValues[$token];
+				}
+				$body = str_replace($tokens[0][$key],$tokenValue,$body);
+		}
+	}
+// final massage body
+	
+echo "after parse <br />";
+// abc MY COMPANY def MY NAME ijk MY NAME lmn MY COMPANY opq TESTTOKEN rst uvw MY NAME xyz.
+echo $body;
